8c60e0e5240bdba82614ca792fd49fc30716a102
hamdimiled <hamdismiled@gmail.com>
2018-04-16 16:39:51 +0200

    add post model

--- model/post.js
+++ model/post.js
@@ -1,2 +1,23 @@
 
+import mongoose, { Collection } from 'mongoose' ;
 
+  //create a schema
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+    uid:{
+        type : String ,
+        required:true
+    },
+    title :{
+        type: String
+    },
+    body:{
+        type : String
+    }
+},
+{collection: 'post' , timestamps:true}
+);
+
+
+//create and export model
+export default mongoose.model('post',postSchema) ;
